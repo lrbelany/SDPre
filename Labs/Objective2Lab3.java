e67bef267cb0388df0c74872c5f77bc69f2a46af
Lowell Belany <lrbelany@gmail.com>
2022-10-07 16:45:31 -0700

    finish Objetive2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year = 1983;
+    String Month = "September";
+    String Weekend = "True";
+
+    System.out.println("Birth year: 1983");
+    System.out.println("Month: September");
+    System.out.println("Weekend: Friday and True");
+    System.out.println("What are weekends?");
+
+  }
+}
