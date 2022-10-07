d406594e455d216da5cb9f72c81eb8a4d7c0c736
Lowell Belany <lrbelany@gmail.com>
2022-10-07 12:33:13 -0700

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Lowell Belany";
+    int age = 39;
+    String state = "California";
+
+    System.out.println("name: Lowell Belany Age: 39 State: California");
+  }
+}
