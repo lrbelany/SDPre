945e90df8ca46294e25e6774eeb1a06a9f8b981c
Lowell Belany <lrbelany@gmail.com>
2022-10-07 19:42:11 -0700

    finish Objective3Lab3 For real this time

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -1,7 +1,9 @@
 public class Objective3Lab3 {
   public static void main(String[] args) {
     int num1 = 1 + (int)(Math.random() * 6);
+    int num2 = 1 + (int)(Math.random() * 6);
+    int num3 = 1 + (int)(Math.random() * 6);
 
-    System.out.println(num1);
+    System.out.println(num1+num2+num3);
   }
 }
