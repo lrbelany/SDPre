addf738c7236744e5e8ba748e754e5c76d8a72c7
Lowell Belany <lrbelany@gmail.com>
2022-10-07 20:48:49 -0700

    ok moving on from Objective3Lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -1,9 +1,15 @@
 public class Objective3Lab3 {
   public static void main(String[] args) {
+
     int num1 = 1 + (int)(Math.random() * 6);
     int num2 = 1 + (int)(Math.random() * 6);
     int num3 = 1 + (int)(Math.random() * 6);
 
-    System.out.println(num1+num2+num3);
+System.out.println(num1);
+System.out.println(num2);
+System.out.println(num3);
+
+int result = (num1 + num2 + num3);
+    System.out.println(result);
   }
 }
