6931ad77c8a42b8c7a78b06dcde0647398fc9dac
Lowell Belany <lrbelany@gmail.com>
2022-10-07 19:21:43 -0700

    finish Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,10 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double num1 = 3 * 3, num2 = 7 * 7, num3 = Math.sqrt(num1 + num2);
+
+    System.out.println(num3);
+
+
+
+  }
+}
