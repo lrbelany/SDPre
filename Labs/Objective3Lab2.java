b540de1a92142a247e763daf340a2858f89e6bfa
Lowell Belany <lrbelany@gmail.com>
2022-10-08 15:36:52 -0700

    finished for real Objective3Lab2

--- Labs/Objective3Lab2.java
+++ Labs/Objective3Lab2.java
@@ -2,7 +2,7 @@ public class Objective3Lab2 {
   public static void main(String[] args) {
     double num1 = 3 * 3, num2 = 7 * 7, num3 = Math.sqrt(num1 + num2);
 
-    System.out.println(num3);
+    System.out.println("The hypotenuse of a triangle wit sides 3.0 and 7.0 is "+ num3);
 
 
 
