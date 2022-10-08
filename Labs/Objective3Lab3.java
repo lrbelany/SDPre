fd2900e34dd7f90de80cc9813e19fffac2dff877
Lowell Belany <lrbelany@gmail.com>
2022-10-08 16:17:15 -0700

    Absolutely done with Objective3Lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -4,12 +4,9 @@ public class Objective3Lab3 {
     int num1 = 1 + (int)(Math.random() * 6);
     int num2 = 1 + (int)(Math.random() * 6);
     int num3 = 1 + (int)(Math.random() * 6);
-
-System.out.println(num1);
-System.out.println(num2);
-System.out.println(num3);
-
 int result = (num1 + num2 + num3);
-    System.out.println(result);
+
+
+    System.out.println(num1 + " + " + num2 + " + " + num3 + " = " + result);
   }
 }
