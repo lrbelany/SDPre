70347b1f5f180280a9ba3bf71966a3e776617c93
Lowell Belany <lrbelany@gmail.com>
2022-10-07 18:33:47 -0700

    finish Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,14 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+double num1 = 3, num2 = 7, num3 = 8, average;
+
+
+double num5 = (num1 + num2 + num3) / num1;
+
+System.out.println(num5);
+
+
+
+
+  }
+}
