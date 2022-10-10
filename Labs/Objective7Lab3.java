f3d1c11c0c92705bec573a1b399f066b4e65b5d1
Lowell Belany <lrbelany@gmail.com>
2022-10-09 21:57:52 -0700

    finished Objective7Lab3 tricky tricky!

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,20 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    double counter = 1;
+    double remainder;
+
+
+while(counter <= 20) {
+
+remainder = counter % 2;
+if ( remainder == 0 ) {
+  System.out.println(counter+" Is even Babyieeeee!");
+}
+
+else {
+  System.out.println(counter+" This number is so odd it hurts my feelings...  :(..)");
+}
+    counter = counter + 1;
+   }
+  }
+}
