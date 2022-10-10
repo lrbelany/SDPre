ece173cac472d549a7ab79834d95d28760718866
Lowell Belany <lrbelany@gmail.com>
2022-10-09 21:39:37 -0700

    finished Objective7Lab2 for real for real

--- Labs/Objective7Lab2.java
+++ Labs/Objective7Lab2.java
@@ -4,30 +4,11 @@ public class Objective7Lab2 {
 
     while(counter <= 20) {
 
-System.out.println(0);
-System.out.println(1);
-System.out.println(2);
-System.out.println(3);
-System.out.println(4);
-System.out.println(5);
-System.out.println(6);
-System.out.println(7);
-System.out.println(8);
-System.out.println(9);
-System.out.println(10);
-System.out.println(11);
-System.out.println(12);
-System.out.println(13);
-System.out.println(14);
-System.out.println(15);
-System.out.println(16);
-System.out.println(17);
-System.out.println(18);
-System.out.println(19);
-System.out.println(20);
+System.out.println(counter);
+
 
 counter = counter + 1;
-break;
+
     }
   }
 }
