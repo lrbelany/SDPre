553c76410933f41cf42446f1ba1c251812cdc4fa
Lowell Belany <lrbelany@gmail.com>
2022-10-09 19:41:44 -0700

    finish Objective7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,24 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+
+System.out.println(10);
+System.out.println(9);
+System.out.println(8);
+System.out.println(7);
+System.out.println(6);
+System.out.println(5);
+System.out.println(4);
+System.out.println(3);
+System.out.println(2);
+System.out.println(1);
+
+
+
+      counter = counter - 1;
+      break;
+    }
+  }
+}
