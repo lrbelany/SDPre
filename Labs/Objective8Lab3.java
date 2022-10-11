58d98a7e796e480f2ccc5accd85694259973e96e
Lowell Belany <lrbelany@gmail.com>
2022-10-10 21:54:01 -0700

    finished Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,24 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+
+int remainder;
+
+{
+for(int counter =1; counter <= 20; counter++){
+
+remainder = counter % 2;
+
+  if (remainder == 0) {
+    System.out.println(counter+" Is even Stevens'");
+  }
+else{
+  System.out.println(counter+" so add and peculiar");
+      }
+
+   }
+
+}
+
+      }
+    }
