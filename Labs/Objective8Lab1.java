4540bf78086e98e466b8096d5a57a271d88ed754
Lowell Belany <lrbelany@gmail.com>
2022-10-10 20:44:14 -0700

    finished Objective8Lab1 and I hate curly braces.....

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,20 @@
+public class Objective8Lab1 {
+ public static void main(String[] args) {
+
+
+for(int counter = 0; counter <= 10; counter++) {
+  System.out.println(counter);
+}
+
+{
+  int counter = 10;
+while(counter > 0) {
+System.out.println(counter);
+ counter--;
+  }
+}
+
+
+ }
+
+}
